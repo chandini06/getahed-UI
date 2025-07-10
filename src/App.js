ffbeba7a840c8323b6ea80a32eda7f7458323d0d
dhanyasri06 <dhanyasri246@gmail.com>
2025-07-10 17:56:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 
 import Dashboard from './pages/Dashboard';
+import LearningPage from './pages/LearningPage';
 import Myjourney from './pages/Myjourney';
 
 import './App.css';
@@ -14,12 +15,15 @@ function App() {
     <Router>
       <div className="main-layout">
         <Navbar />
+
         <div className="content-layout" style={{ display: 'flex' }}>
           <Sidebar />
           <div className="page-content w-full p-6">
             <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/Myjourney" element={<Myjourney />} />
+              <Route path="/" element={<Navigate to="/dashboard" />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/learning" element={<LearningPage />} />
+              <Route path="/myjourney" element={<Myjourney />} />
             </Routes>
           </div>
         </div>
